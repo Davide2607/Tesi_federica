e1e31e881932fd914ee165876ba26f4aa68c276f
federicamato00 <134133171+federicamato00@users.noreply.github.com>
2025-05-06 11:05:13 +0200

    Update neptune_init.py

--- scripts/neptune_init.py
+++ scripts/neptune_init.py
@@ -4,7 +4,7 @@ import neptune
 def init_neptune():
 # Inizializza Neptune
     run = neptune.init_run(
-        project="famato/tesi",  # Sostituisci con il tuo project name
-        api_token="eyJhcGlfYWRkcmVzcyI6Imh0dHBzOi8vYXBwLm5lcHR1bmUuYWkiLCJhcGlfdXJsIjoiaHR0cHM6Ly9hcHAubmVwdHVuZS5haSIsImFwaV9rZXkiOiIxNjcxZDE1OC01YmVjLTRiZGUtYTZhYi01YzdiM2MxNTcxMjIifQ=="            # Sostituisci con il tuo API token
+        project="",  # Sostituisci con il tuo project name
+        api_token=""            # Sostituisci con il tuo API token
     )
     return run
